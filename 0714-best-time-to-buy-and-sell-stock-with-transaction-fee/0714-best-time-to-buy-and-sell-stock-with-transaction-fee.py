744d516e63f795fc3e05550c41f90b2d0f123d21
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2024-11-21 22:26:34 +0530

    Time: 283 ms (29.11%), Space: 26.7 MB (28.66%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,27 +1,18 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
+        dp = [[0,0] for i in range(len(prices) + 1)]
         size = len(prices)
-        dp = [[-1,-1] for i in range(size)]
-        def helper(index, canBuy):
-            if index == size:
-                return 0
+        for i in range(size - 1, -1, -1):
             max_profit = 0
-            if dp[index][canBuy] != -1:
-                return dp[index][canBuy]
-            if canBuy:
-                can_buy = -prices[index] + helper(index + 1,1 - canBuy)
-                cannot_buy = helper(index + 1,canBuy)
-                max_profit = max(can_buy, cannot_buy)
-            else:
-                can_sell = (prices[index] - fee) + helper(index + 1,1 - canBuy)
-                cannot_sell = helper(index + 1,canBuy)
-                max_profit = max(can_sell, cannot_sell)
-            dp[index][canBuy] = max_profit
-            return dp[index][canBuy]
 
-        answer = helper(0, 1)
-        return answer
-
-
-
-
+            for canBuy in range(2):
+                if canBuy:
+                    take = -prices[i] + dp[i + 1][1 - canBuy]
+                    not_take = dp[i + 1][canBuy]
+                    max_profit = max(take, not_take)
+                else:
+                    take = (prices[i] - fee) + dp[i + 1][1 - canBuy]
+                    not_take = dp[i + 1][canBuy]
+                    max_profit = max(take, not_take)
+                dp[i][canBuy] = max_profit
+        return dp[0][1]
