c7b6791ee0e5ef597e87514a883e5804cab805fa
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2024-11-27 11:30:20 +0530

    Time: 8 ms (11.74%), Space: 17.8 MB (5.58%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,19 +1,29 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        row = set()
-        col = set()
-
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
+        # First pass solution
+        # 1. Traverse if zero than mark first element of the row and column to be zero
+        row,col = len(matrix), len(matrix[0])
+        check = 1
+        for i in range(row):
+            for j in range(col):
                 if matrix[i][j] == 0:
-                    row.add(i)
-                    col.add(j)
-        
-        for i in range(len(matrix)):
-            for j in range(len(matrix[0])):
-                if i in row or j in col:
-                    matrix[i][j] = 0
+                    matrix[i][0] = 0
+
+                    if j != 0:
+                        matrix[0][j] = 0
+                    else:
+                        check = 0
+
+        for i in range(1, row):
+            for j in range(1,col):
+                if matrix[i][j] != 0:
+                    if matrix[i][0] == 0 or matrix[0][j] == 0:
+                        matrix[i][j] = 0
+
+        if matrix[0][0] == 0:
+            for i in range(col):
+                matrix[0][i] = 0
+        if check == 0:
+            for j in range(row):
+                matrix[j][0] = 0
         
