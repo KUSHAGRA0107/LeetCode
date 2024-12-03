ac8720879784cda5f5105d6a30c5c1eda33da55f
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2024-12-03 14:16:32 +0530

    Time: 6 ms (15.65%), Space: 47.9 MB (18.35%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,28 +1,29 @@
 class Solution {
-    public int getminSum(int i, int j, int [][] grid, int [][] dp){
-        int m = grid.length;
-        int n = grid[0].length;
-        if(i < 0 || j < 0 || j >= n || i >= m){
-            return Integer.MAX_VALUE;
-        }
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-        if(i == m - 1 && j == n - 1){
-            return grid[m - 1][n - 1];
-        }
-        int right = getminSum(i, j + 1, grid,dp);
-        int down = getminSum(i + 1, j,grid,dp);
-        dp[i][j] =  grid[i][j] + Math.min(right, down);
-        return dp[i][j];
-    }
     public int minPathSum(int[][] grid) {
-        int [][] dp = new int[grid.length][grid[0].length];
-        for(int i = 0; i < grid.length; i++){
-            for(int j = 0;j < grid[0].length;j++){
-                dp[i][j] = -1;
+        int row = grid.length;
+        int col = grid[0].length;
+        int [][] table = new int[row][col];
+        table[row - 1][col - 1] = grid[row - 1][col - 1];
+        for(int column = col - 2; column >= 0; column--){
+            table[row - 1][column] = grid[row - 1][column]
+            +
+            table[row - 1][column + 1];
+        }
+
+        for(int i = row - 2; i >= 0; i--){
+            for(int j = col - 1; j >= 0; j--){
+                int right = Integer.MAX_VALUE;
+                int left = Integer.MAX_VALUE;  
+
+                if(j + 1 < col){
+                    right = table[i][j + 1];
+                }
+                if(i + 1 < row){
+                    left = table[i + 1][j];
+                }
+                table[i][j] = grid[i][j] + Math.min(left, right);
             }
         }
-        return getminSum(0,0,grid,dp);
+        return table[0][0];
     }
 }
