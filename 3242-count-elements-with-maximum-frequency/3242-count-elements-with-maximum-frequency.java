681e6017b5e9954c94a9856dc343588b80e92ab5
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2024-12-14 16:10:41 +0530

    Time: 1 ms (99.6%), Space: 41.6 MB (98.21%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
@@ -1,10 +1,14 @@
 class Solution {
     public int maxFrequencyElements(int[] nums) {
+
+        // initialisation of the variable
+
         int answer = 0;
         int maximumValue = -1;
         int getMaximum = -1;
         int size = nums.length;
 
+        // get maximum element to get size of frequency array
         for(int i = 0;i < size;i++){
             if(nums[i] > getMaximum){
                 getMaximum = nums[i];
@@ -13,16 +17,20 @@ class Solution {
 
         int [] frequency = new int[getMaximum + 1];
 
+        // creation of frequency array
         for(int i = 0; i < size; i++){
             frequency[nums[i]]++;
         }
 
+        // get the maximum frequency that occcured in the array
         for(int i = 0; i < frequency.length;i++){
             if(frequency[i] > maximumValue){
                 maximumValue = frequency[i];
             }
         }
 
+        // now all the element having the maximumValue as their frequency 
+        // will get their frequency added up
         for(int i = 0; i < frequency.length; i++){
             if(frequency[i] == maximumValue){
                 answer += maximumValue;
