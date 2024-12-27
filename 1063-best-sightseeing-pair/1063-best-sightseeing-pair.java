153810f873ee0e4278b11ee55d750fe46fed2344
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2024-12-27 20:55:48 +0530

    Time: 4 ms (89.46%), Space: 53.7 MB (24.59%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.java
@@ -1,14 +1,11 @@
 class Solution {
     public int maxScoreSightseeingPair(int[] values) {
-        int [] prefix = new int[values.length];
         int max = 0;
-        prefix[0] = values[0];
-        for(int i = 1; i < values.length ;i++){
-            prefix[i] = Math.max(prefix[i - 1], values[i] + i);
-        }           
-        for(int j = 1; j < values.length; j++){
-            int curr = values[j] - j; 
-            max = Math.max(max, prefix[j - 1] + curr);
+        int prev = values[0] + 0;
+        int curr = 0;
+        for(int i = 1; i < values.length; i++){
+            max = Math.max(max, values[i] - i + prev);
+            prev = Math.max(prev, values[i] + i);
         }
         return max;
     }
