964a41b6a5bd931d9446234d7e25f4d6f8aff938
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2024-12-30 21:48:46 +0530

    Time: 6 ms (98.66%), Space: 43.3 MB (76.51%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.java
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.java
@@ -1,18 +1,17 @@
 class Solution {
-    static int mod = 1000000007;
-    public static int getCount(int low, int high, int zero, int one, int length, int [] dp){
-        if(length > high) return 0;
-        if(dp[length] != -1) return dp[length];
-        int result = 0;
-        if(length >= low) result = 1;
-        result += getCount(low, high, zero, one, length + zero, dp) + getCount(low, high, zero, one, length + one, dp);
-        dp[length] = result % mod;
-        return dp[length];
-    }
     public int countGoodStrings(int low, int high, int zero, int one) {
-        int [] dp = new int[high + 1];
-        for(int i = 0;i < dp.length; i++) dp[i] = -1;
-        int answer = getCount(low, high, zero, one, 0, dp);
-        return answer;
+        int [] dp_table = new int[high + 1];
+        dp_table[high] = 0;
+        int result = 0;
+        int mod = 1000000007;
+
+        for(int i = high; i >= 0; i--){
+            result = 0;
+            if(low <= i && i <= high) result = 1;
+            if(i + zero <= high) result += dp_table[i + zero];
+            if(i + one <= high) result += dp_table[i + one];
+            dp_table[i] = result % mod;
+        }
+        return dp_table[0];
     }
 }
