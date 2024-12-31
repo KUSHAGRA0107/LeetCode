241b6c4cf9c6e23d69be4d638022e3fde4b1201a
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2024-12-31 16:29:52 +0530

    Time: 0 ms (100%), Space: 42.1 MB (63.34%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,18 +1,29 @@
 class Solution {
+    public static void reverse(int [] arr){
+        int i = 0;
+        int j = arr.length - 1;
+        while(i < j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i += 1;
+            j -= 1;
+        }
+    }
     public void rotate(int[][] matrix) {
         int n = matrix.length;
-        int [][] temp = new int[n][n];
-
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n;j++){
-                temp[j][n - i - 1] = matrix[i][j];
+        // Transpose of the Matrix
+        for(int i = 0; i < n - 1;i++){
+            for(int j = i + 1; j < n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
 
+        // Reverse the matrix row-wise
         for(int i = 0; i < n; i++){
-            for(int j = 0; j < n;j++){
-                matrix[i][j] = temp[i][j];
-                } 
-            }
+            reverse(matrix[i]);
+        }
     }
 }
