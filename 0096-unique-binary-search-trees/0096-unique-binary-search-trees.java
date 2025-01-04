3a12e2a4444c00e3ac9a9f542412536171500c2b
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2025-01-04 14:47:47 +0530

    Time: 0 ms (100%), Space: 40.4 MB (43.06%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -9,10 +9,23 @@ class Solution {
         dp[n] = total;
         return total;
     }
+    public int solveTabulation(int n){
+        int [] answer = new int[n + 1];
+        for(int i = 0; i <= n ; i++) answer[i] = 1;
+
+        for(int nodes = 2; nodes <= n; nodes++){
+            int total = 0;
+            for(int root = 1; root <= nodes; root++){
+                total += answer[root - 1] * answer[nodes - root];
+            }
+            answer[nodes] = total;
+        }
+        return answer[n];
+    }
     public int numTrees(int n) {
         int [] dp = new int[n + 1];
         Arrays.fill(dp,-1);
-        int answer = getCount(n, dp);
+        int answer = solveTabulation(n);
         return answer;
     }
 }
