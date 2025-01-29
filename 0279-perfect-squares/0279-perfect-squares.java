82cb035247904ffc3c5a3c769d6fddc27b0bd61a
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2025-01-29 14:59:49 +0530

    Time: 34 ms (62.28%), Space: 43 MB (54.26%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -9,10 +9,22 @@ class Solution {
         dp[number] = min;
         return min;
     }
+    public int tabulate(int n){
+        int [] table = new int[n + 1];
+        Arrays.fill(table, Integer.MAX_VALUE);
+        table[0] = 0;
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j * j <= i; j++){
+                table[i] = Math.min(table[i], 1 + table[i - (j * j)]);
+            }
+        }
+        return table[n];
+    }
     public int numSquares(int n) {
-        int [] dp = new int[n + 1];
-        Arrays.fill(dp, -1);
-        int min = getMinimum(n, dp);
-        return min;
+        // int [] dp = new int[n + 1];
+        // Arrays.fill(dp, -1);
+        // int min = getMinimum(n, dp);
+        // return min;
+        return tabulate(n);
     }
 }
