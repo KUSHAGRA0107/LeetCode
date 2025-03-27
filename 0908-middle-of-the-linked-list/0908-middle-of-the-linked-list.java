0caea4200188abddd3ce7ede6861c5fdb1bb938c
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2025-03-28 00:08:57 +0530

    Time: 0 ms (100%), Space: 41 MB (86%) - LeetHub

--- 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
+++ 0908-middle-of-the-linked-list/0908-middle-of-the-linked-list.java
@@ -1,24 +1,11 @@
 class Solution {
-    public int getSize(ListNode head){
-        int size = 0;
-        while(head != null){
-            size++;
-            head = head.next;
-        }
-        return size;
-    }
     public ListNode middleNode(ListNode head) {
-        int size = getSize(head);
-        int nodeTobeSearched = (size / 2) + 1;
-        int currNode = 1;
-        ListNode temp = head;
-        while(temp != null && currNode != nodeTobeSearched){
-            temp = temp.next;
-            currNode++;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
-        // if(size % 2 == 0){
-        //     return temp.next;
-        // }
-        return temp;
+        return slow;
     }
 }
