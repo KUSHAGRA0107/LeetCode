f4a7bdfcee2bb45b83c2373baaf90c7af8f11745
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2025-04-14 12:38:36 +0530

    Time: 6 ms (88.43%), Space: 44.9 MB (25.34%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,15 +1,32 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int size = nums.length;
-        int [] dp = new int[size];
-        Arrays.fill(dp, 1);
-        for(int index = 1; index < size; index++){
-            for(int prev = 0; prev < index; prev++){
-                if(nums[prev] < nums[index]){
-                    dp[index] = Math.max(dp[index], dp[prev] + 1);
-                }
+    public int getIndex(List<Integer> temp, int target){
+        int low = 0;
+        int high = temp.size() - 1;
+        int index = -1;
+        while(low <= high){
+            int mid = (low + high) >> 1;
+            if(temp.get(mid) >= target){
+                index = mid;
+                high = mid - 1;
+            }else{
+                low = mid + 1;
             }
         }
-        return Arrays.stream(dp).max().getAsInt();
+        return index;
+    }
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> temp = new ArrayList<>();
+        int length = 0;
+        int size = nums.length;
+        for(int i = 0; i < size; i++){
+            if(temp.size() == 0 || temp.get(temp.size() - 1) < nums[i]){
+                temp.add(nums[i]);
+                length++;
+            }else{
+                int getIdx = getIndex(temp, nums[i]);
+                if(getIdx != -1)temp.set(getIdx, nums[i]);
+            }
+        }
+        return length;
     }
 }
