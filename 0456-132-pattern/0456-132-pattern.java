a58eb98be339d00a37d3f7a91cc1331d0c33d936
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2025-04-19 17:14:09 +0530

    Time: 35 ms (75.3%), Space: 65.4 MB (98.01%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.java
+++ 0456-132-pattern/0456-132-pattern.java
@@ -25,3 +25,19 @@ class Solution {
         }return false;
     }
 }
+
+/*
+    We need to find three value such that nums[i] < nums[j] and nums[i] < nums[k]
+    but at the same time nums[i] < nums[k] for any i < j < k
+    this gives us an intuition
+    that we need to minimize the value of nums[i]
+    for any index k we will find it's previous greater element and
+    any minimum value from i = 0 to i < j such that it should be minimum till that index
+    and also should not be equal to nums[j] and at the same time should be less than
+    that of nums[k]
+
+    minimum can be found with help of minimum prefix array
+    and we need to check if we find any element smaller than top of stack
+    which is greater than the current element and at the same time it should be
+    smaller than both nums[i], nums[j]
+ */
