d7a5c2827e310e7dc69736bc5511a1e1d7406833
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2025-05-09 00:34:07 +0530

    Time: 1 ms (62.54%), Space: 45.8 MB (92.83%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,14 +1,23 @@
 class Solution {
     public int trap(int[] height) {
-        int size = height.length;
-        int [] prefix = new int[size];
-        int [] suffix = new int[size];
         int answer = 0;
-        prefix[0] = height[0];
-        suffix[size - 1] = height[size - 1];
-        for(int i = 1;i<size;i++) prefix[i] = Math.max(prefix[i - 1], height[i]);
-        for(int i = size - 2;i >= 0;i--) suffix[i] = Math.max(suffix[i + 1], height[i]);
-        for(int i = 1; i < size - 1; i++)answer += Math.min(prefix[i], suffix[i]) - height[i];
+        int size = height.length;
+        int [] left = new int[size];
+        left[0] = height[0];
+        
+        int [] right = new int[size];
+        right[size - 1] = height[size - 1];
+
+        for(int i = 1; i < size; i++){
+            left[i] = Math.max(left[i - 1], height[i]);
+        }
+        for(int i = size  - 2; i >= 0; i--){
+            right[i] = Math.max(right[i + 1], height[i]);   
+        }
+
+        for(int i = 0; i < size; i++){
+            answer += Math.min(right[i], left[i]) - height[i];
+        }
         return answer;
     }
 }
