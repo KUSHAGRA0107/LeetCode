f5ebf3ee8c8a766d082b56c6e2c937bd784b978b
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2025-05-09 00:35:06 +0530

    Time: 1 ms (62.54%), Space: 46.2 MB (67.29%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,21 +2,17 @@ class Solution {
     public int trap(int[] height) {
         int answer = 0;
         int size = height.length;
-        int [] left = new int[size];
-        left[0] = height[0];
-        
+        int left_max = 0;
         int [] right = new int[size];
         right[size - 1] = height[size - 1];
 
-        for(int i = 1; i < size; i++){
-            left[i] = Math.max(left[i - 1], height[i]);
-        }
         for(int i = size  - 2; i >= 0; i--){
             right[i] = Math.max(right[i + 1], height[i]);   
         }
 
         for(int i = 0; i < size; i++){
-            answer += Math.min(right[i], left[i]) - height[i];
+            left_max = Math.max(left_max, height[i]);
+            answer += Math.min(right[i], left_max) - height[i];
         }
         return answer;
     }
