43385f1593ec6770fc3380067074cad473899c8d
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2025-05-09 00:38:36 +0530

    Time: 1 ms (62.54%), Space: 46.7 MB (28.92%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -2,17 +2,21 @@ class Solution {
     public int trap(int[] height) {
         int answer = 0;
         int size = height.length;
-        int left_max = 0;
-        int [] right = new int[size];
-        right[size - 1] = height[size - 1];
+        int i = 0;
+        int j = size - 1;
+        int lmax = height[0];
+        int rmax = height[size - 1];
 
-        for(int i = size  - 2; i >= 0; i--){
-            right[i] = Math.max(right[i + 1], height[i]);   
-        }
-
-        for(int i = 0; i < size; i++){
-            left_max = Math.max(left_max, height[i]);
-            answer += Math.min(right[i], left_max) - height[i];
+        while(i <= j){
+            if(lmax <= rmax){
+                lmax = Math.max(lmax, height[i]);
+                answer += lmax - height[i];
+                i++;
+            }else{
+                rmax = Math.max(rmax, height[j]);
+                answer += rmax - height[j];
+                j--;
+            }
         }
         return answer;
     }
