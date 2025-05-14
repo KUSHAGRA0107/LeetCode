93345b657697308bb1208f41021ec53acec3066d
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2025-05-14 12:08:11 +0530

    Time: 11 ms (92.29%), Space: 42.5 MB (72.66%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -1,30 +1,23 @@
 class Solution {
-    private int getMincost(int i, int j, int [] nums, int [][] dp){
-        if(i > j) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        int mini = Integer.MAX_VALUE;
-        for(int index = i; index <= j; index++){
-            int answer = nums[j + 1] - nums[i - 1] + getMincost(i, index - 1, nums, dp) + getMincost(index + 1, j, nums, dp);
-            mini = Math.min(mini, answer);
-        }
-        dp[i][j] = mini;
-        return mini;
-    }
     public int minCost(int n, int[] cuts) {
         int size = cuts.length;
         int [][] dp = new int[size + 2][size + 2];
         int [] nums = new int[size + 2];
-        for(int i = 0; i < size; i++){
-            nums[i] = cuts[i];
-            Arrays.fill(dp[i], -1);
-        }
-        for(int i = 0; i < size + 2; i++){
-            Arrays.fill(dp[i], -1);
-        }
+        for(int i = 0; i < size; i++) nums[i] = cuts[i];
         nums[size] = 0;
         nums[size + 1] = n;
         Arrays.sort(nums);
-        int answer = getMincost(1, size, nums, dp);
-        return answer;
+        for(int i = size; i >= 1; i--){
+            for(int j = 1; j <= size; j++){
+                if(i > j) continue;
+                int min = Integer.MAX_VALUE;
+                for(int index = i; index <= j; index++){
+                    int answer = nums[j + 1] - nums[i - 1] + dp[i][index - 1] + dp[index + 1][j];
+                    min = Math.min(min, answer);
+                }
+                dp[i][j] = min;
+            }
+        }
+        return dp[1][size];
     }
 }
