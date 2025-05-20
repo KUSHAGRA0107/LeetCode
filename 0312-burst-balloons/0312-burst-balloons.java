7a881e4f90ecd71f825fbb63322b46190f571242
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2025-05-20 12:09:39 +0530

    Time: 31 ms (96.62%), Space: 42.8 MB (42.56%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -1,30 +1,22 @@
 class Solution {
-    public int getMax(int i, int j, int [] nums, int [][] dp){
-        if(i > j) return 0;
-        if(dp[i][j] != -1) return  dp[i][j];
-        int maxi = Integer.MIN_VALUE;
-        for(int index = i; index <= j; index++){
-            maxi = Math.max(maxi,
-            nums[i - 1] * nums[index] * nums[j + 1] + getMax(i, index - 1, nums, dp) + getMax(index + 1 , j, nums, dp));
-        }
-        dp[i][j] = maxi;
-        return maxi;
-    }
     public int maxCoins(int[] nums) {
         int size = nums.length;
         int [] temp = new int[size + 2];
-        for(int i = 0; i < size; i++){
-            temp[i + 1] = nums[i];
-        }
+        for(int i = 0; i < size; i++)temp[i + 1] = nums[i];
         temp[0] = 1;
         temp[size + 1] = 1;
-        int [][] dp = new int[size + 2][size + 2];
-        for(int i = 0; i < size + 2; i++){
-            for(int j = 0; j < size + 2; j++){
-                dp[i][j] = -1;
+        int [][] table = new int[size + 2][size + 2];
+        for(int i = size ; i > 0; i--){
+            for(int j = 0; j <= size; j++){
+                if(i > j) continue;
+                int maxi = Integer.MIN_VALUE;
+                for(int index = i; index <= j; index++){
+                    maxi = Math.max(maxi, temp[i - 1] * temp[index] * temp[j + 1] + table[i][index - 1] +
+                    table[index + 1][j]);
+                }
+                table[i][j] = maxi;
             }
         }
-        int answer = getMax(1, size, temp, dp);
-        return answer;
+        return table[1][size];
     }
 }
