fb5115e990bdabee4e032042259e4c0149f0a3b0
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2025-05-22 13:00:12 +0530

    Time: 23 ms (41.43%), Space: 44.8 MB (67.98%) - LeetHub

--- 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
+++ 0712-minimum-ascii-delete-sum-for-two-strings/0712-minimum-ascii-delete-sum-for-two-strings.java
@@ -1,30 +1,22 @@
 class Solution {
-    public int helper(int i, int j, String s1, String s2, int [] s1Arr, int [] s2Arr, int m, int n, int [][] dp){
-        if(i == m && j == n) return 0;
-        if(i == m) return s2Arr[j];
-        if(j == n) return s1Arr[i];
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s1.charAt(i) == s2.charAt(j)) return helper(i + 1, j + 1, s1, s2, s1Arr, s2Arr, m, n, dp);
-        return dp[i][j] = Math.min((int)s1.charAt(i) + helper(i + 1, j, s1, s2, s1Arr, s2Arr, m, n, dp), (int) s2.charAt(j) + helper(i, j + 1, s1, s2, s1Arr, s2Arr, m, n, dp));
-    }
     public int minimumDeleteSum(String s1, String s2) {
-        int m = s1.length();
-        int n = s2.length();
-        int [] s1Arr = new int[m];
-        s1Arr[m - 1] = (int) s1.charAt(m - 1);
-        for(int i = m - 2; i >= 0; i--){
-            s1Arr[i] = (int) s1.charAt(i) + s1Arr[i + 1];
+        int [][] dp = new int[s1.length() + 1][s2.length() + 1];
+        for(int i = 1; i <= s2.length(); i++){
+            dp[0][i] = dp[0][i - 1] + (int)s2.charAt(i - 1);
         }
-        int [] s2Arr = new int[n];
-        s2Arr[n - 1] = (int) s2.charAt(n - 1);
-        for(int i = n - 2; i >= 0; i--){
-            s2Arr[i] = (int) s2.charAt(i) + s2Arr[i + 1];
+        for(int i = 1; i <= s1.length(); i++){
+            dp[i][0] = dp[i - 1][0] + (int)s1.charAt(i - 1);
         }
-        int [][] dp = new int[m][n];
-        for(int i = 0; i < m; i++){
-            Arrays.fill(dp[i], -1);
+
+        for(int i = 1; i <= s1.length(); i++){
+            for(int j = 1; j <= s2.length(); j++){
+                if(s1.charAt(i - 1) == s2.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }else{
+                    dp[i][j] = Math.min(s1.charAt(i - 1) + dp[i - 1][j], s2.charAt(j - 1) + dp[i][j - 1]);
+                }
+            }
         }
-        int answer = helper(0, 0, s1, s2, s1Arr, s2Arr, m, n, dp);
-        return answer;
+        return dp[s1.length()][s2.length()];
     }
 }
