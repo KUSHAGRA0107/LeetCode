834f861b6b307713d875f25696cdfde7fb23c5b9
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2025-08-04 23:56:15 +0530

    Time: 1 ms (100%), Space: 44.4 MB (83.6%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -9,21 +9,25 @@
  * }
  */
 class Solution {
-    public void add(ListNode l1, ListNode l2, ListNode ptr, int carry){
-        if(l1 == null && l2 == null && carry == 0) return;
-        int value = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + carry;
-        int sum = value % 10;
-        carry = value / 10;
+    public void helper(ListNode head1, ListNode head2, ListNode ptr, int carry)
+    {
+        if(head1 == null && head2 == null && carry == 0) return;
+        int head1_val = (head1 == null) ? 0 :  head1.val;
+        int head2_val = (head2 == null) ? 0 :  head2.val;
+        int summation = head1_val + head2_val + carry;
+        int sum = summation % 10;
+        carry = summation / 10;
         ListNode node = new ListNode(sum);
         ptr.next = node;
-        ptr = ptr.next;
-        add(l1 == null ? null : l1.next,l2 == null ? null : l2.next,ptr, carry);
+        ptr = node;
+        helper(head1 == null ? null : head1.next, head2 == null ? null : head2.next, ptr, carry);
         return;
     }
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(0);
         ListNode ptr = head;
-        add(l1, l2, ptr, 0);
+        int carry = 0;
+        helper(l1, l2, ptr, carry);
         return head.next;
     }
 }
