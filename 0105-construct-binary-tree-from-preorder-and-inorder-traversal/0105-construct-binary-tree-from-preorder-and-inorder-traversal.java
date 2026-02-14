635dc601e15653e0b74dbe79eee428203071151a
Kushagra Tandon <116801700+KUSHAGRA0107@users.noreply.github.com>
2026-02-14 12:52:07 +0530

    Time: 2 ms (64.01%), Space: 46.2 MB (71.24%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -1,23 +1,21 @@
 class Solution {
-    public int searchRoot(int rootvalue, int [] inorder, int left, int right){
-        for(int i = left; i <= right; i++){
-            if(inorder[i] == rootvalue) return i;
-        }
-        return -1;
-    }
-    public TreeNode buildRoot(int [] rootIndex, int left, int right, int [] preorder, int [] inorder){
+    public TreeNode buildRoot(int [] rootIndex, int left, int right, int [] preorder, int [] inorder, HashMap<Integer, Integer> map){
         if(rootIndex[0] == preorder.length || left > right) return null;
         TreeNode root = new TreeNode(preorder[rootIndex[0]]);
-        int index = searchRoot(preorder[rootIndex[0]], inorder, left, right);
+        int index = map.get(preorder[rootIndex[0]]);
         rootIndex[0]++; 
-        root.left = buildRoot(rootIndex, left, index - 1, preorder, inorder);
-        root.right = buildRoot(rootIndex, index + 1, right, preorder, inorder);;
+        root.left = buildRoot(rootIndex, left, index - 1, preorder, inorder, map);
+        root.right = buildRoot(rootIndex, index + 1, right, preorder, inorder, map);
         return root;
     }
     public TreeNode buildTree(int[] preorder, int[] inorder) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < inorder.length; i++){
+            map.put(inorder[i], i);
+        }
         int [] rootIndex = new int[1];
         rootIndex[0] = 0;
-        TreeNode root = buildRoot(rootIndex, 0, inorder.length, preorder, inorder);
+        TreeNode root = buildRoot(rootIndex, 0, inorder.length, preorder, inorder,map);
         return root;
     }
 }
